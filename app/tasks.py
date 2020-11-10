fbed159411f932d3a0368ba8e7596fa7d5d7352c
Karl <karlecnal@protonmail.com>
2020-11-09 19:33:31 -0500

    pruning questions background task & session route update

---
+++ app/tasks.py
@@ -0,0 +1,18 @@
+import time
+from flask_sqlalchemy import SQLAlchemy
+from app import db
+from app.models import User, Question
+timetoPrune=60 * 60
+def prune():
+    while True:
+        print("Pruning Question Database...")
+        pruned=False
+        for i in db.session.query(Question).all():
+            if i.prune():
+                print("Pruning "+i)
+                db.session.remove(i)
+                pruned=True
+        if pruned:
+            db.session.commit()
+        print("Pruning complete, waiting ",timetoPrune/60," minutes.")
+        time.sleep(timetoPrune)
